5ea67da6bab866eee105334b81cae4aab2c3ef88
refinedPath <208536365+RefinedPath@users.noreply.github.com>
2026-08-20 00:40:28 -0400

    Wire PDO connection through container from env config

--- public/index.php
+++ public/index.php
@@ -3,13 +3,36 @@
 declare(strict_types=1);
 
 use DI\ContainerBuilder;
+use Dotenv\Dotenv;
 use Slim\Factory\AppFactory;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
 require __DIR__ . '/../vendor/autoload.php';
 
+$dotenv = Dotenv::createImmutable(__DIR__ . '/..');
+$dotenv->load();
+$dotenv->required(['DB_HOST', 'DB_PORT', 'DB_NAME', 'DB_USER', 'DB_PASS']);
+
 $containerBuilder = new ContainerBuilder();
+
+$containerBuilder->addDefinitions([
+  PDO::class => function (): PDO {
+    $dsn = sprintf(
+      'pgsql:host=%s;port=%s;dbname=%s',
+      $_ENV['DB_HOST'],
+      $_ENV['DB_PORT'],
+      $_ENV['DB_NAME'],
+    );
+
+    return new PDO($dsn, $_ENV['DB_USER'], $_ENV['DB_PASS'], [
+      PDO::ATTR_ERRMODE             => PDO::ERRMODE_EXCEPTION,
+      PDO::ATTR_DEFAULT_FETCH_MODE  => PDO::FETCH_ASSOC,
+      PDO::ATTR_EMULATE_PREPARES    => false,
+    ]);
+  },
+]);
+
 $container = $containerBuilder->build();
 
 AppFactory::setContainer($container);
@@ -22,6 +45,13 @@ $app->get('/', function (Request $request, Response $response): Response {
 
 $app->get('/hello', function (Request $request, Response $response): Response {
   $response->getBody()->write('Hello from MapApp');
+  return $response;
+});
+
+$app->get('/db-check', function (Request $request, Response $response) use ($container): Response {
+  $pdo = $container->get(PDO::class);
+  $version = $pdo->query('SELECT version()')->fetchColumn();
+  $response->getBody()->write((string) $version);
   return $response;
 });
 
