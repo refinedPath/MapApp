efa281283661e67fd000639269de0718e887db14
refinedPath <208536365+RefinedPath@users.noreply.github.com>
2026-08-24 13:12:44 -0400

    Remove generated docblocks from migrations

--- migrations/20260820204430_create_users_table.php
+++ migrations/20260820204430_create_users_table.php
@@ -7,20 +7,9 @@ use Phinx\Util\Literal;
 
 final class CreateUsersTable extends AbstractMigration
 {
-    /**
-     * Change Method.
-     *
-     * Write your reversible migrations using this method.
-     *
-     * More information on writing migrations is available here:
-     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
-     *
-     * Remember to call "create()" or "update()" and NOT "save()" when working
-     * with the Table class.
-     */
     public function change(): void
     {
-        $this->table('users',['id' => false, 'primary_key' => ['id']])
+        $this->table('users', ['id' => false, 'primary_key' => ['id']])
             ->addColumn('id', 'uuid', ['null' => false])
             ->addColumn('email', Literal::from('citext'), ['null' => false])
             ->addColumn('password_hash', 'string', ['limit' => 255, 'null' => false])
